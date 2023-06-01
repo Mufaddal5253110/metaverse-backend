e9c3dae3fdb83ef38ecee8eeab644ac8bbd9da4d
Mufaddal5253110 <mufaddalshakir55@gmail.com>
2023-06-02 01:28:15 +0530

    updated

--- routes/nft.js
+++ routes/nft.js
@@ -33,7 +33,7 @@ nftRouter.get("/api/v1/nft/:id", async (req, res) => {
 nftRouter.get("/api/v1/nft/:roomId/:imageId/imageUrl", async (req, res) => {
   try {
     const nft = await Nft.findOne({roomId:req.params.roomId,imageId:req.params.imageId});
-    res.json(nft["imageUrl"]);
+    res.json(nft);
   } catch (e) {
     res.status(500).json({ error: e.message });
   }
@@ -42,7 +42,7 @@ nftRouter.get("/api/v1/nft/:roomId/:imageId/imageUrl", async (req, res) => {
 nftRouter.get("/api/v1/nft/:roomId/:imageId/openSeaUrl", async (req, res) => {
   try {
     const nft = await Nft.findOne({roomId:req.params.roomId,imageId:req.params.imageId});
-    res.json(nft["openSeaUrl"]);
+    res.json(nft);
   } catch (e) {
     res.status(500).json({ error: e.message });
   }
