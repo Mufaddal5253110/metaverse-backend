4a515c45d4e29a845b0f5f1fdbbd49ecacdce9ce
Mufaddal5253110 <mufaddalshakir55@gmail.com>
2023-06-02 00:39:51 +0530

    updated

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const bodyParser  = require("body-parser");
+
 const nftRouter = require("./routes/nft");
 
 const DB = "mongodb+srv://mufaddalshakir55:mufaddalshakir55@cluster0.ydq7nvw.mongodb.net/?retryWrites=true&w=majority";
 const app = express();
 // middleware
-app.use(express.json());
-app.use(nftRouter)
+// app.use(express.json());
+app.use(bodyParser.json());
+
 
 mongoose
   .connect(DB)
@@ -20,3 +23,5 @@ mongoose
 app.listen(8080, "localhost", () => {
   console.log(`connected at port ${8080}`);
 });
+
+app.use(nftRouter)
--- routes/nft.js
+++ routes/nft.js
@@ -2,24 +2,23 @@ const express = require("express");
 const nftRouter = express.Router();
 const Nft = require("../models/nft.js");
 
-
-nftRouter.post("/api/v1/add-nft",async (req, res) => {
-    try {
-        let nft = new Nft(req.body);
-        nft = await nft.save();
-        res.json(nft);
-    } catch (e) {
-        res.status(500).json({ error: e.message });
-    }
+nftRouter.post("/api/v1/add-nft", async (req, res) => {
+  try {
+    let nft = new Nft(req.body);
+    nft = await nft.save();
+    res.json(nft);
+  } catch (e) {
+    res.status(500).json({ error: e.message });
+  }
 });
 
-nftRouter.get("/api/v1/nft",async (req, res) => {
-    try {
-        const nft = Nft.find({});
-        res.json(nft);
-    } catch (e) {
-        res.status(500).json({ error: e.message });
-    }
+nftRouter.get("/api/v1/nft", async (req, res) => {
+  try {
+    const nft = Nft.find();
+    res.json(nft);
+  } catch (e) {
+    res.status(500).json({ error: e.message });
+  }
 });
 
 module.exports = nftRouter;
